e739f90003ce0b376858e3ad41d886ab4cc2e145
Asish Mehata <mehataasish@gmail.com>
2025-06-01 23:35:18 +0545

    useState used

---
+++ react-state-class/src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export default function Counter() {
+    let [count, setCount] = useState(0);
+
+    let incCount = ()  => {
+        setCount(count+1);
+        console.log(count);
+    }
+    return (
+        <>
+            <h3>count = {count}</h3>
+            <button onClick={incCount}>Increase Count</button>
+        </>
+    )
+}
