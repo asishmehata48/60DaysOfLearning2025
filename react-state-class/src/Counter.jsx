b6a9dba36308d8ff33b48083aa4bdad617e5839e
Asish Mehata <mehataasish@gmail.com>
2025-06-03 19:56:21 +0545

    passing function as parameter in initialization

--- react-state-class/src/Counter.jsx
+++ react-state-class/src/Counter.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react"
 
+
+function init () {
+    console.log("init was executed.")
+    return Math.random();
+}
+
+
 export default function Counter() {
-    let [count, setCount] = useState(0);
+    let [count, setCount] = useState(init); //initialization
     console.log("Component is rendered");
-    console.log(`count: ${count}`);
 
     let incCount = ()  => {
         setCount((currCount) => {
             return currCount+1;
         });
-        setCount((currCount) => {
-            return currCount+1;
-        });
-        setCount(25);
-    }
+    };
     return (
         <>
             <h3>count = {count}</h3>
