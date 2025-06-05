ebef86f8b6dfa13fc23ac5b484dad978f6c79dbc
Asish Mehata <mehataasish@gmail.com>
2025-06-05 22:18:49 +0545

    import Example.jsx

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Counter from './Counter.jsx'
 import TodoList from './TodoList.jsx'
+import Example from './Example.jsx'
 
 function App() {
 
   return (
     <>
-      <TodoList />
+      <Example />
     </>
   )
 }
