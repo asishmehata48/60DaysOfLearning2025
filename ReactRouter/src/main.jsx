fde2540815f28ac28e62fa96e41fc1fb69580b7b
Asish Mehata <mehataasish@gmail.com>
2025-06-14 08:54:07 +0545

    ReactRouter Main.jsx

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Layout from './Layout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
 import './index.css'
-import App from './App.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
