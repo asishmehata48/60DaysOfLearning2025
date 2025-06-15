0bbca59c384825c8958dfcf2434e0b12402622ca
Asish Mehata <mehataasish@email.com>
2025-06-15 21:45:06 +0545

    RouterBrowser

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Layout from './Layout';
@@ -8,27 +8,36 @@ import './index.css'
 import Contact from './components/Contact/Contact';
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "contact",
-        element: <Contact />
-      }
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
 
+const router = createBrowserRouter(
+  createRoutesFromElements (
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />}></Route>
+      <Route path='about' element={<About />}></Route>
+      <Route path='contact' element={<Contact />}></Route>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
