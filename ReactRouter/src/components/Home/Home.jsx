1d5c202c3616bd463abed4b70309729cbdbc0e0d
Asish Mehata <mehataasish@email.com>
2025-06-15 16:18:30 +0545

    Home.jsx added

--- ReactRouter/src/components/Home/Home.jsx
+++ ReactRouter/src/components/Home/Home.jsx
@@ -8,8 +8,8 @@ export default function Home() {
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
                         <h2 className="text-4xl font-bold sm:text-5xl">
-                            Download Now
-                            <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
+                            Instagram
+                            <span className="hidden sm:block text-4xl">Free Social Media App</span>
                         </h2>
 
                         <Link
@@ -32,15 +32,15 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img className="w-96" src="https://t4.ftcdn.net/jpg/11/30/89/31/360_F_1130893149_gYXXfR1snXYFO26R944XXEGy3Kc6kxF9.webp" alt="image1" />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src="https://t4.ftcdn.net/jpg/11/53/50/25/240_F_1153502547_EOdRUyUmbiVrpGowhhLKrYnQkVFsjAvr.jpg" alt="image2" />
             </div>
 
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Used icons</h1>
         </div>
     );
 }
