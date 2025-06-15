312acfb10c134da562edbd6905f93aa9e863d44b
Asish Mehata <mehataasish@email.com>
2025-06-15 22:47:07 +0545

    GIthub added

---
+++ ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+function Github() {
+
+    const [data, setData] = useState([]);
+
+    useEffect (() => {
+        fetch('https://api.github.com/users/asishmehata48')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    }, [])
+  return (
+    <div className="flex flex-wrap items-center text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+        <div className="w-full md:w-1/2">
+            <h1>Username: {data.login}</h1>
+            <h3 className="text-xl text-black">Bio: {data.bio}</h3>
+            <p className="text-2xl text-blue-400">GitHub Followers: {data.followers}</p>
+        </div>
+        <div className="w-full md:w-1/2">
+            <img src={data.avatar_url} alt="Git Picture" width={300} />
+        </div>
+    </div>
+
+  );
+}
+
+export default Github;
