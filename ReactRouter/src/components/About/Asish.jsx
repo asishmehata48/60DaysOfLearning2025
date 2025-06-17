7afa32e3940217254e8555d43487a9c470af2d18
Asish Mehata <mehataasish@gmail.com>
2025-06-17 19:22:10 +0545

    Asish.jsx created

---
+++ ReactRouter/src/components/About/Asish.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Asish() {
+  return (
+    <div className='py-16 mt-10'>
+        <h2 className="flex items-center justify-center p-2">
+            Profile Pictures
+        </h2>
+        <div className='flex space=x=4 w-75'>
+            <img src="https://scontent.fbwa4-1.fna.fbcdn.net/v/t39.30808-6/501656023_685329084310019_2737508617221223319_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG0h4G0U9ZQto80Ot-fK6jeNp3C7BCdczs2ncLsEJ1zOx3gM2BioWFvsXdsCJndj8pXdZFCOxhCbZ3kXztK16M_&_nc_ohc=q_LkYY4UlvkQ7kNvwG9YiAY&_nc_oc=Admr4qZYy7SXqcnkMgBYjFc6C1vK-qQvOj55f1f1xvb9Y0KuV3H7ihgxZ5mXrhiXEZKy65WaK3JWl01V0X4aylWU&_nc_zt=23&_nc_ht=scontent.fbwa4-1.fna&_nc_gid=IXXuqZUeQ2gaSgPgw80a4g&oh=00_AfPwyl7HJn1DYxtu_-YIAci-tqbWNgWRK_azQ9wwhcayRA&oe=68562325" alt="profile" className='w-full rounded-2xl shadow-lg '/>
+            <img src="https://scontent.fbwa4-1.fna.fbcdn.net/v/t39.30808-6/493229247_662556886587239_275516912501402091_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHGUiYWJF0Pk07tihd9wpZwMZebYXC1NaQxl5thcLU1pJXsHkdfY0ECaR6xWaODutpQMPUQKZHj-GvjbohvGEcR&_nc_ohc=Z2LbUpkKUPMQ7kNvwHaAf4x&_nc_oc=Adm5KcXp4ZYdSBI0XtOcUlGVQvWZN6laeOeA0fvVmCmFacFpyxid6gY3o43EnN6znjlGof-zoo41rlCAZ-S48GkB&_nc_zt=23&_nc_ht=scontent.fbwa4-1.fna&_nc_gid=vOz-xR4bi3aF63ee4WZxUw&oh=00_AfMQjvWTv7qMAC1jTP1SjhmqfnW986MGYn7gJQP1-7FvWg&oe=685629F5" alt="2"
+            className='w-full rounded-2xl shadow-lg ' />
+            <img src="https://scontent.fbwa4-1.fna.fbcdn.net/v/t39.30808-6/485747257_633719022804359_9037178835609182051_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeGMZe2uvk0t85ADYU7IVaq0t8bFCWO2smW3xsUJY7ayZSjfVK-9eMxedry0s0f5F27YKVMhkQJ3u0dGkrFOOI0j&_nc_ohc=UL2hagJAAt8Q7kNvwEO7KB3&_nc_oc=AdkmBOUOgiNiIwlrb5S7Z3e7Rjt9FgLi_VvpzJLaPWGi_cDIVBSFhjGe8ycpXVubRCrpu2TW90AxRIHrBYUIofd3&_nc_zt=23&_nc_ht=scontent.fbwa4-1.fna&_nc_gid=0fxXxGF4HBr0cNL8igrpXA&oh=00_AfMPrWUmh4bn43uR7rOH5fkPhrIje1k-3ZWTMffxXp-KLA&oe=68564000" alt="3" 
+            className='w-full rounded-2xl shadow-lg '/>
+            <img src="https://scontent.fbwa4-1.fna.fbcdn.net/v/t39.30808-6/481452344_620804570762471_8378955817241901117_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeGY7z4h6VS_8RtGnH1FYr4MaTrPJuVPymNpOs8m5U_KY844ksAmMZDntRIbtHGNiCvJGRuwbixpoyUBeMRWmqB2&_nc_ohc=m-Lq0vsFilQQ7kNvwEuohFQ&_nc_oc=Adk02_5lj_l13BCEfiREuC9s0fiJzgs2rc9yh_ie_Di3Yaji_fDFY8GoMDgkn6Znw_0-wz4zb9bljZXmyTieX3Sl&_nc_zt=23&_nc_ht=scontent.fbwa4-1.fna&_nc_gid=VnmytOlRp8BWvDAgiavC9w&oh=00_AfM_W-rJX6Oc5BSEejOXm5pwxWwUMgKVPAzcFmvi1Dz9cw&oe=6856337C" alt="4" 
+            className='w-full rounded-2xl shadow-lg'
+            />
+        </div>
+    </div>
+  );
+}
+
+export default Asish;
