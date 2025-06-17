873b63d3dd771d599d3278a34b97741156848326
Asish Mehata <mehataasish@gmail.com>
2025-06-17 23:51:10 +0545

    Context API

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import UserContextProvider from "./context/UserContextProvider"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <UserContextProvider>
+      <h1>Context-API Project</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
