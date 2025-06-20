8450d8fe7142aaf0313c21dc5debce180843c8da
Asish Mehata <mehataasish@gmail.com>
2025-06-20 23:44:44 +0545

    Navbar made

---
+++ BrandPage/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="https://github.com/anshuopinion/React-10-Projects/blob/project-1/project-1/public/images/brand_logo.png?raw=true" alt="logo" />
+      </div>
+      <ul>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Location</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
