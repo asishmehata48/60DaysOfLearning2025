28b521f68fb85bbae52eb31438f4553c666c6b97
Asish Mehata <mehataasish@gmail.com>
2025-06-21 06:08:32 +0545

    initial commit

---
+++ Portfolio/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css"; 
+
+const Header = () => {
+  return (
+    <header>
+      <a href="#" className="logo">
+        Asish <span className="slate">Mehata</span>.
+      </a>
+
+    </header>
+  );
+};
+
+export default Header;
