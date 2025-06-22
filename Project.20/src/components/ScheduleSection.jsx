4bd9d09f9fbeb0b04f5bde42bbb0be6c9fa412e9
Asish Mehata <mehataasish@gmail.com>
2025-06-22 23:13:56 +0545

    .

--- Project.20/src/components/ScheduleSection.jsx
+++ Project.20/src/components/ScheduleSection.jsx
@@ -2,6 +2,7 @@ import scheduleImage from '../assets/stats.webp'
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
 
+
 const ScheduleSection = () => {
   return (
     <motion.section 
