9c29e209768c7e2187499fcbee4966f48d452a7c
Asish Mehata <mehataasish@gmail.com>
2025-06-22 23:38:58 +0545

    .

--- Project.20/src/components/PricingSection.jsx
+++ Project.20/src/components/PricingSection.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
 
+
 const PricingSection = () => {
   const [productCount, setProductCount] = useState(1)
   
