41b91dda5c9059ec1c24db1605c6eda8de19c82d
Asish Mehata <mehataasish@gmail.com>
2025-06-27 00:05:26 +0545

    process.argv used

--- node.2/script.js
+++ node.2/script.js
@@ -1,7 +1,13 @@
-let n = 5;
+// let n = 5;
 
-for (let i=0; i<n; i ++) {
-    console.log("hello, ", i);
+// for (let i=0; i<n; i ++) {
+//     console.log("hello, ", i);
+// }
+
+// console.log("bye");
+
+let args = process.argv;
+
+for (let i=0; i <args.length; i ++) {
+    console.log("hello to, ", args[i]);
 }
-
-console.log("bye");
