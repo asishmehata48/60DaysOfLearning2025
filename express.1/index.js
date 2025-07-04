e260661e5ecc44b39ae8870e8c55e56ba57463a1
Asish Mehata <mehataasish@gmail.com>
2025-07-04 23:22:20 +0545

    request send to server

--- express.1/index.js
+++ express.1/index.js
@@ -8,3 +8,11 @@ let port = 3000;
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
+
+
+app.use((req, res) => {
+    // console.log(req);
+    console.log("request received.");
+    let code = "<h1>Fruits<h1/> <ul><li>Apple</li><li>Mango</li><li>Orange</li></ul>"
+    res.send(code);
+})
