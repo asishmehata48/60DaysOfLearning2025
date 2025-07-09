87970a12accfb751cfdca8f9995cc193fd43290e
Asish Mehata <mehataasish@gmail.com>
2025-07-09 21:06:45 +0545

    ejs rendered and path required.

--- EJS/index.js
+++ EJS/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-const port = 8080;
+const port = 3000;
 
+app.set("view engine", "ejs");
+app.set("views",  path.join(__dirname, "/views"));
+
+
+app.get("/", (req,res) => {
+    res.render("home.ejs");
+});
+
+
+app.get("/hello", (req,res) => {
+    res.send("Hello");
+});
 
 app.listen(port, () => {
     console.log( `listening on port ${port}`)
-})
+});
+
