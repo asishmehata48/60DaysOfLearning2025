c73d109e392e5c0496b6927f2a562f968f80b6ac
Asish Mehata <mehataasish@gmail.com>
2025-07-12 23:05:39 +0545

    .ejs rendered.

---
+++ EJS.3/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
