eaf49b5672b1b57a5aced0ed7041b8266afc647c
Asish Mehata <mehataasish@gmail.com>
2025-07-18 23:45:40 +0545

    server side code

---
+++ getConcept/backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/register", (req,res) => {
+    let {user, password} = req.query;
+    res.send(`standard GET response. Welcome ${user}!`);
+});
+
+app.post("/register" , (req,res) =>{
+    res.send(`standard POST response. `);
+});
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+});
