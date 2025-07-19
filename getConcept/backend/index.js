d1a71aeeda9f1397b7231b16082ec8e53bc8a138
Asish Mehata <mehataasish@gmail.com>
2025-07-19 23:28:04 +0545

    parsing post reqeusts

--- getConcept/backend/index.js
+++ getConcept/backend/index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
 app.get("/register", (req,res) => {
     let {user, password} = req.query;
     res.send(`standard GET response. Welcome ${user}!`);
 });
 
 app.post("/register" , (req,res) =>{
-    const { user, password } = req.body;
-    res.send(`standard POST response. `);
+    let {user, password} =  req.body;
+    res.send(`standard POST response. Welcome ${user}!`);
 });
 
 app.listen(port, () => {
